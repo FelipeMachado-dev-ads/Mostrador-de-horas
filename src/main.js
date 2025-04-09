058e1b59c032c41c445813a0de17eb6f6c5a3a7d
Felipe Machado Dev <felipe.dev.ads@gmail.com>
2025-04-09 00:32:22 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,21 +8,21 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
-        img.src = '/src/img/manha.png'
+        img.src = './src/img/manha.png'
         document.body.style.backgroundColor = '#fdc060'
         msg.style.color = '#425151'
         titulo.style.color = '#425151'
         footer.style.color = '#425151'
 
     } else if (hora >= 12 && hora <= 18) {
-        img.src = '/src/img/tarde.png'
+        img.src = './src/img/tarde.png'
         document.body.style.backgroundColor = '#fef2dd'
         msg.style.color = '#49403c'
         titulo.style.color = '#49403c'
         footer.style.color = '#49403c'
 
     } else {
-        img.src = '/src/img/noite.png'
+        img.src = './src/img/noite.png'
         document.body.style.backgroundColor = '#101d25'
         msg.style.color = '#f2f2f2'
         titulo.style.color = '#f2f2f2'
